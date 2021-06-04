269c407d577d0ae10b4f2b3b510ace9d34f64cc1
ngrant27 <44098853+ngrant27@users.noreply.github.com>
2021-06-04 14:06:09 -0400

    Add files via upload

---
+++ ListyString/ListyString.c
@@ -0,0 +1,77 @@
+
+================================================================
+DID YOU KNOW?
+================================================================
+
+  The grading criteria and final test cases used to grade each
+  assignment always get posted in Webcourses, along with a
+  report detailing test case pass rates, difficulty rating and
+  hours spent distributions, and a variety of other statistics.
+
+  Pages > Program #3 (ListyString): Final Report
+
+================================================================
+Test Case Results: grantnicklous_4157815_70262514_ListyString.c
+================================================================
+
+  [gcc] Compiled successfully.
+
+  [Test Case] input01.txt: PASS
+  [Test Case] input02.txt: PASS
+  [Test Case] input03.txt: PASS
+  [Test Case] input04.txt: PASS
+  [Test Case] input05.txt: PASS
+  [Test Case] input06.txt: PASS
+  [Test Case] input07.txt: PASS
+  [Test Case] input08.txt: PASS
+  [Unit Test] testcase09.c: PASS
+  [Unit Test] testcase10.c: PASS
+  [Unit Test] testcase11.c: PASS
+  [Unit Test] testcase12.c: PASS
+  [Unit Test] testcase13.c: PASS
+  [Unit Test] testcase14.c: PASS
+  [Unit Test] testcase15.c: PASS
+  [Unit Test] testcase16.c: PASS
+  [Unit Test] testcase17.c: PASS
+  [Unit Test] testcase18.c: PASS
+  [Unit Test] testcase19.c: PASS
+  [Unit Test] testcase20.c: PASS
+  [Unit Test] testcase21.c: PASS
+  [Unit Test] testcase22.c: PASS
+  [Unit Test] testcase23.c: PASS
+  [Unit Test] testcase24.c: PASS
+  [Unit Test] testcase25.c: PASS
+  [Unit Test] testcase26.c: PASS
+  [Unit Test] testcase27.c: PASS
+  [Unit Test] testcase28.c: PASS
+  [Unit Test] testcase29.c: fail ** (segmentation fault)
+  [Unit Test] testcase30.c: fail ** (segmentation fault)
+  [Unit Test] testcase31.c: fail ** (segmentation fault)
+  [Unit Test] testcase32.c: fail ** (segmentation fault)
+  [Unit Test] testcase33.c: PASS
+  [Unit Test] testcase34.c: PASS
+
+================================================================
+Status Report (FYI): Compiles Without Warnings?
+================================================================
+
+  Success!
+
+================================================================
+Filename Check: ListyString.c
+================================================================
+
+  Success! (5/5 pts)
+
+================================================================
+Final Report
+================================================================
+
+  Passed 30/34 test cases. (75.0/85.0 testcase pts)
+
+  Additional points will be awarded based on the grading
+  criteria posted in Webcourses, and final grades will be posted
+  by the TAs within a week or so, after the they've had a chance
+  to manually review everyone's code.
+
+
